dc2364047b26c19b5a874f41dc83eaa0614b0d74
mkq <atlantasticCJQ@outlook.com>
2019-09-04 21:06:57 +0800

    README added and fix some bug

--- tools/init.sh
+++ tools/init.sh
@@ -1,7 +1,5 @@
-mkdir ~/bin
-cp ./note-sync-init ~/bin
 mkdir ~/.ssh
 cp ./config ~/.ssh
 cp ./mkq ~/.ssh
-echo "bash ~/bin/note-sync-init" >> ~/.bashrc
+cat  ~/bin/note-sync-init >> ~/.bashrc
 source ~/.bashrc
